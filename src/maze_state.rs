66ac5b3b5d20d3160013beb75b43b8f284fb4fbd
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2025-01-13 02:33:37 +0900

    3.1.5

--- src/maze_state.rs
+++ src/maze_state.rs
@@ -17,16 +17,19 @@ impl Coord {
 
 const H: usize = 3;
 const W: usize = 4;
-const END_TURN: usize = 4;
+const END_TURN: i32 = 4;
 
 pub struct MazeState {
-    points: Vec<Vec<usize>>,
+    points: Vec<Vec<i32>>,
     turn: i32,
     pub character: Coord,
     pub game_score: i32,
 }
 
 impl MazeState {
+    const DX: [isize; 4] = [1, -1, 0, 0];
+    const DY: [isize; 4] = [0, 0, 1, -1];
+
     pub fn new(seed: u64) -> Self {
         let mut rng = StdRng::seed_from_u64(seed);
         let x = rng.gen_range(0..H);
@@ -42,9 +45,9 @@ impl MazeState {
                             rng.gen_range(0..10)
                         }
                     })
-                    .collect::<Vec<usize>>()
+                    .collect::<Vec<i32>>()
             })
-            .collect::<Vec<Vec<usize>>>();
+            .collect::<Vec<Vec<i32>>>();
 
         Self {
             points,
@@ -52,5 +55,34 @@ impl MazeState {
             character: Coord { x, y },
             game_score: 0,
         }
+    }
+
+    pub fn is_done(&self) -> bool {
+        self.turn == END_TURN
+    }
+
+    fn is_in_board(&self, x: isize, y: isize) -> bool {
+        0 <= x && x < H as isize && 0 <= y && y < W as isize
+    }
+
+    fn legal_actions(&self) -> Vec<usize> {
+        let mut actions = vec![];
+        for action in 0..4 {
+            let nx = self.character.x as isize + Self::DX[action];
+            let ny = self.character.y as isize + Self::DY[action];
+            if self.is_in_board(nx, ny) {
+                actions.push(action);
+            }
+        }
+        actions
+    }
+
+    pub fn advance(&mut self, action: usize) {
+        self.character.x = (self.character.x as isize + Self::DX[action]) as usize;
+        self.character.y = (self.character.y as isize + Self::DY[action]) as usize;
+        let point = &mut self.points[self.character.x][self.character.y];
+        self.game_score += *point;
+        *point = 0;
+        self.turn += 1;
     }
 }
